faa8128b1684f4672bd8dba319541c82de8d417f
ATAT6666 <1099167749@qq.com>
2019-08-20 20:31:28 +0800

    作业

---
+++ js基础/js/suanshuti.js
@@ -0,0 +1,35 @@
+//第一题
+//alert((123+(45*78)) / (34+543) - (9*(324 - 34)));
+
+
+
+//第二题
+// var a1 = parseInt(prompt("请输入一个度数"));
+// var a2 = (9/5)*a1+32;
+// console.log("华氏度是"+a2);
+
+
+
+//第三题
+//alert(Math.pow(23 + Math.pow(5,7) / 45,2));
+
+
+//第四题
+// var a1 = parseInt(prompt("请输入边长"));
+// var a2 = ((3 * Math.sqrt(3)) / 2) *(Math.pow(a1,2));
+// alert(a2);
+
+//第五题
+// var a2 = ((1000 % 7 + 2) % 7);
+// alert("1000天后星期"+ a2);
+// var a1 = parseInt(prompt("请输入数字"));
+// var a3 = (a1%7);
+// alert(a3);
+
+//第六题
+// var a = parseInt(prompt("请输入一个三位数"));
+// var b = parseInt(a%10);
+// var c = parseInt((a%100)/10);
+// var d = parseInt((a/100));
+// alert((b+c+d));
+// 
